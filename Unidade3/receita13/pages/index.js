e8632761b5487b7058093a44d372c3e39b5df6c4
ErickBezerrar <erickbrtrindade@gmail.com>
2023-08-25 10:23:50 -0300

    receita13 -> Trying other time

--- Unidade3/receita13/pages/index.js
+++ Unidade3/receita13/pages/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { About } from "./about";
-import { Contact } from "./contact";
-import styles from '../style/style.module.css';
-
-export default function Home() {
+export default function Principal(){
     return (
-        <div className={styles.container}>
-            <h2>
-                Viva a Jesus!!
-            </h2>
-            <About />
-            <Contact />
-            <img width='375'src="https://arquivos.info.ufrn.br/arquivos/20202020440d2c7471879040a453273e7/IMG_20200423_120408753-01.jpeg"></img>
-            <p>Obrigado por iluminar o caminho do código e transformar o complexo em algo compreensível. Sua orientação é a faísca que acende minha paixão pela programação.</p>
-        </div>
+ 
+       <div>
+ 
+          <h1>Nova Página</h1>
+ 
+       </div>
+ 
     )
-}
+ 
+ }
+
+ export function MariaPrea(){
+    return (
+ 
+       <h2>Morreu Maria Preá...</h2>
+ 
+    )
+ 
+ }
