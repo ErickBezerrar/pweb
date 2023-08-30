9984c2f95779fa34cae69ef7e980e2b914cda5d5
ErickBezerrar <erickbrtrindade@gmail.com>
2023-08-30 10:40:42 -0300

    receita13 -> página home completa, estilizações, fade in e fade out

--- Unidade3/receita13/pages/index.js
+++ Unidade3/receita13/pages/index.js
@@ -7,8 +7,16 @@ export default function Principal(){
     return (
         <div>
             <Navigation></Navigation>
-            
+            <h1 className={styles.bemvindo}>Bem-vindo ao <br></br>Nosso Website!</h1>
+            <h2 className={styles.somosempresa}>
+               Somos uma empresa dedicada <br></br>a fornecer soluções inovadoras...
+            </h2>
+                <p className={styles.paragrafo}>"Somos uma empresa inovadora dedicada a oferecer<br></br> soluções tecnológicas para seus desafios.<br></br> Nossa paixão é transformar ideias em realidade por meio de<br></br> desenvolvimento de software, design criativo e estratégias digitais.<br></br> Trabalhamos para impulsionar o seu sucesso com eficiência e qualidade."</p>
+                    <div>
+
+                    </div>
         </div>
+        
     )
 }
 
