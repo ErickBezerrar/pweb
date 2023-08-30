d75ae5a529846dcce84a8ce3117d550f8b05fd2b
ErickBezerrar <erickbrtrindade@gmail.com>
2023-08-30 14:31:52 -0300

    receita13 -> página home completa!

--- Unidade3/receita13/pages/index.js
+++ Unidade3/receita13/pages/index.js
@@ -1,5 +1,5 @@
-import React from 'react'; 
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 import styles from '../styles/index.module.css';
 import Navigation from '../pages/navigation';
 
@@ -7,14 +7,17 @@ export default function Principal(){
     return (
         <div>
             <Navigation></Navigation>
-            <h1 className={styles.bemvindo}>Bem-vindo ao <br></br>Nosso Website!</h1>
-            <h2 className={styles.somosempresa}>
-               Somos uma empresa dedicada <br></br>a fornecer soluções inovadoras...
-            </h2>
-                <p className={styles.paragrafo}>"Somos uma empresa inovadora dedicada a oferecer<br></br> soluções tecnológicas para seus desafios.<br></br> Nossa paixão é transformar ideias em realidade por meio de<br></br> desenvolvimento de software, design criativo e estratégias digitais.<br></br> Trabalhamos para impulsionar o seu sucesso com eficiência e qualidade."</p>
-                    <div>
-
-                    </div>
+            <div className={styles.divinfo}>
+                <h1 className={styles.bemvindo}>Bem-vindo ao <br></br>Nosso Website!</h1>
+                <img className={styles.imagem} src='https://www.proyectum.com/sistema/wp-content/uploads/2020/02/Empresa-de-tecnolog%C3%ADa.jpg'></img>
+            </div>
+                <h2 className={styles.somosempresa}>
+                    Somos uma empresa dedicada <br></br>a fornecer soluções inovadoras...
+                </h2>
+            <div className={styles.divinfo2}>
+                <img className={styles.imagem2} src='https://agendor-blog-uploads.s3.sa-east-1.amazonaws.com/2018/03/15112351/estrategia-de-crescimento-de-uma-empresa1.jpg'></img>
+                <p className={styles.paragrafo}>"Somos uma empresa inovadora dedicada a oferecer soluções tecnológicas para seus desafios. Nossa paixão é transformar ideias em realidade por meio de desenvolvimento de software, design criativo e estratégias digitais. Trabalhamos para impulsionar o seu sucesso com eficiência e qualidade."</p>
+            </div>
         </div>
         
     )
--- Unidade3/receita13/pages/navigation.js
+++ Unidade3/receita13/pages/navigation.js
@@ -6,9 +6,9 @@ export default function Navigation() {
     return (
         <div className={styles.cabecalho}>
             <Link href="/">Home</Link>
-            <Link href="/paginasobre/sobre">About Us</Link>
-            <Link href="/paginacontato/contato">Contato</Link>
-            <Link href="/paginacadastro/cadastro">Cadastro</Link>
+            <Link href="/paginasobre/sobre">About</Link>
+            <Link href="/paginacontato/contato">Contact</Link>
+            <Link href="/paginacadastro/cadastro">Register</Link>
         </div>
     );
 }
