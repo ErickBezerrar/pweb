2c9727bb31ebf5b77148d3648860023e0ef4d808
ErickBezerrar <erickbrtrindade@gmail.com>
2023-08-30 15:25:20 -0300

    receita13 -> página cadastro

--- Unidade3/receita13/pages/paginacadastro/cadastro.js
+++ Unidade3/receita13/pages/paginacadastro/cadastro.js
@@ -1,27 +1,35 @@
 import React from 'react'; 
 import Link from 'next/link'
 import Navigation from '../../pages/navigation';
-//import styles from '../styles/cadastro.module.css';
+import styles from '../../styles/cadastro.module.css';
 
 export default function Cadastro() {
     return (
         <div>
             <Navigation></Navigation>
-            <div className="container">
-                <h1>Cadastre-se</h1>
-                <form action="#" method="post">
-                    <label htmlFor="name">Nome:</label>
-                    <input type="text" id="name" name="name" required />
+            <div className={styles.wrapper}>
+                <form action=''>
+                    <h1 className={styles.nameLogin}>Login</h1>
+                    <div className={styles.inputbox}>
+                        <input type='text' placeholder='Username' required></input>
+                    </div>
+                    <div className={styles.inputbox}>
+                        <input type='password' placeholder='Password' required></input>
+                    </div>
 
-                    <label htmlFor="email">E-mail:</label>
-                    <input type="email" id="email" name="email" required />
+                    <div className={styles.rememberForgot}>
+                        <label><input type='checkbox'></input> Remember me</label>
+                        <a href='#'>Forgot password?</a>
+                    </div>
 
-                    <label htmlFor="password">Senha:</label>
-                    <input type="password" id="password" name="password" required />
 
-                    <button type="submit">Cadastrar</button>
+                    <button type='submit' className={styles.bnt}>Login</button>
+
+                    <div className={styles.registerLink}>
+                        <p>Don't have an account? <a href='#'>Register</a></p>
+                    </div>
                 </form>
-            </div>
+            </div>    
         </div>
     );
 }
