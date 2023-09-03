0f4b0fa196724e9d741602cf622ff5a7d1e39f16
ErickBezerrar <erickbrtrindade@gmail.com>
2023-09-03 10:01:36 -0300

    receita13 -> concluída

--- Unidade3/receita13/pages/index.js
+++ Unidade3/receita13/pages/index.js
@@ -6,7 +6,7 @@ import Navigation from '../pages/navigation';
 export default function Principal(){
     return (
         <div>
-            <Navigation />
+            <Navigation/>
             <div className={styles.divinfo}>
                 <h1 className={styles.bemvindo}>Bem-vindo ao <br></br>Nosso Website!</h1>
                 <img className={styles.imagem} src='https://www.proyectum.com/sistema/wp-content/uploads/2020/02/Empresa-de-tecnolog%C3%ADa.jpg'></img>
@@ -18,8 +18,20 @@ export default function Principal(){
                 <img className={styles.imagem2} src='https://agendor-blog-uploads.s3.sa-east-1.amazonaws.com/2018/03/15112351/estrategia-de-crescimento-de-uma-empresa1.jpg'></img>
                 <p className={styles.paragrafo}>"Somos uma empresa inovadora dedicada a oferecer soluções tecnológicas para seus desafios. Nossa paixão é transformar ideias em realidade por meio de desenvolvimento de software, design criativo e estratégias digitais. Trabalhamos para impulsionar o seu sucesso com eficiência e qualidade."</p>
             </div>
+            <MensagemPersonalizada className={styles.mensagempersonalizada} m="Neste exemplo, a função MensagemPersonalizada recebe a mensagem como um parâmetro e a exibe dentro de uma tag."/>
         </div>
         
     )
 }
 
+function MensagemPersonalizada(props) {
+
+    const { m } = props;
+
+    return (
+      <div>
+        <p>{m}</p>
+      </div>
+    );
+  }
+  
