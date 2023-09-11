863c6683188abd83832c0c836aea087f774f8b6b
ErickBezerrar <erickbrtrindade@gmail.com>
2023-09-11 10:27:53 -0300

    receita14 -> updates

---
+++ Unidade3/receita14/pages/movies.js
@@ -0,0 +1,35 @@
+export default function Movies({data}){
+
+    return (
+
+        <div>
+
+            <div>
+
+            {data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  )}               
+
+            </div>
+
+        </div>
+
+    )
+
+}
+
+export async function getServerSideProps(context){
+
+    const res = await fetch(`http://www.omdbapi.com/?apikey=e0d35ba4&s=bagdad`)
+
+    const data = await res.json()
+
+    return {
+  
+        props: {
+  
+            data
+  
+        }
+  
+    }
+  
+}
